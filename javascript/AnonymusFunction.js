425f8f967f174c2bf0c1563d6662f12ebb11bd30
shivpatel027 <patelshiv1701@gmail.com>
2024-05-23 21:17:04 -0500

    Anonyous functions, callbacks, and loops added

---
+++ javascript/AnonymusFunction.js
@@ -0,0 +1,19 @@
+function square(n) {
+    return n*n;
+}
+
+function cube(n) {
+    return n*n*n;
+}
+
+function sumOfSomething(a, b, fn) {
+    let var1 = fn(a);
+    let var2 = fn(b);
+
+    return var1 + var2;
+}
+
+let ans = sumOfSomething(12, 13, function (n) {
+    return n*n*n;
+});
+console.log(ans);
